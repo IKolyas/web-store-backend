011e026287a5e4a1761c5da93ec8d8fdff6df1fe
ivan kov <63105949+IKolyas@users.noreply.github.com>
2021-02-14 16:17:18 +0500

    c

--- server_django/settings.py
+++ server_django/settings.py
@@ -173,9 +173,7 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 MEDIA_URL = '/media/'
 
