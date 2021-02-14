829caaa4b0e7051f3c462eee423dfe9fa5ca8e2b
ivan kov <63105949+IKolyas@users.noreply.github.com>
2021-02-14 17:18:11 +0500

    BIG COMMIT

--- web_store_backend/asgi.py
+++ web_store_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for server_django project.
+ASGI config for web_store_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_store_backend.settings')
 
 application = get_asgi_application()
