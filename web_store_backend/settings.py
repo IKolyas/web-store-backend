5d60cecccc7f3024127505b084b6907c7146b34c
ivan kov <63105949+IKolyas@users.noreply.github.com>
2021-03-01 01:47:12 +0500

    сщьь

--- web_store_backend/settings.py
+++ web_store_backend/settings.py
@@ -88,15 +88,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'web_store_backend.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
 DATABASES = {
     'default': {
 
